2960b483e87ff745513e6f1fa9da9dcb66db88f9
wentam <wentam42@gmail.com>
2025-05-29 15:10:11 -0400

    test

--- build_docs.sh
+++ build_docs.sh
@@ -13,8 +13,16 @@ cat "$t" >> $tmpfile2
 typst compile $tmpfile2 docs/README-light.pdf
 
 tmpfile3=$(mktemp)
-cat "$t" > $tmpfile3
-typst compile $tmpfile3 docs/README-embed.pdf
+echo "#set text(fill: black)" > $tmpfile3
+cat "$t" >> $tmpfile3
+typst compile $tmpfile3 docs/README-embed-light.pdf
+
+tmpfile4=$(mktemp)
+echo '#set page(fill: rgb("0d1117"))' > $tmpfile4
+echo "#set text(fill: white)" >> $tmpfile4
+cat "$t" >> $tmpfile4
+typst compile $tmpfile4 docs/README-embed-dark.pdf
+
 
 for d in docs/*.pdf; do
   svg="${d%.*}.svg"
