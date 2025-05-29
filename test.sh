2c5ca8f2103bc122a32f92d956bbf5fdd6f62f13
wentam <wentam42@gmail.com>
2025-05-29 13:44:48 -0400

    test

--- test.sh
+++ test.sh
@@ -7,7 +7,7 @@ done
 
 for d in *.pdf; do
   svg="${d%.*}.svg"
-  inkscape --without-gui --file="$d" --export-plain-svg="$svg"
+  inkscape --without-gui --file="$d" --pdf-poppler --export-plain-svg="$svg"
 
   # TODO add <rect width="100%" height="100%" fill="#ffffff"/> inside svg element at top
 done
