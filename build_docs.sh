94997aaf119e82abeac4784d84356b9e69b1af90
wentam <wentam42@gmail.com>
2025-05-29 13:58:24 -0400

    test

--- build_docs.sh
+++ build_docs.sh
@@ -1,8 +1,5 @@
-# for d in *.pdf ; do inkscape --without-gui --file=$d --export-plain-svg=${d%.*}.svg --export-background=#FFFFFF ; done
-
 mkdir -p doc_build/
 
-# #set text(fill: white)
 t="README.typ"
 tmpfile=$(mktemp)
 echo "#set text(fill: white)" > $tmpfile
@@ -13,6 +10,4 @@ typst compile $t doc_build/README-light.pdf
 for d in doc_build/*.pdf; do
   svg="${d%.*}.svg"
   inkscape --without-gui --file="$d" --pdf-poppler --export-plain-svg="$svg"
-
-  # TODO add <rect width="100%" height="100%" fill="#ffffff"/> inside svg element at top
 done
